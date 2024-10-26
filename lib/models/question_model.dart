d0600fcb0e85192c1419dc833d138c7f1e7db662
Diyaz <diyaz.murfin1@gmail.com>
2024-10-26 22:33:21 +0500

    feat: add selectedVariant field in QuestionModel

--- lib/models/question_model.dart
+++ lib/models/question_model.dart
@@ -1,9 +1,11 @@
 class QuestionModel {
   final String question;
   final Map<int, String> variants;
+  int? selectedVariant;
 
   QuestionModel({
     required this.question,
     required this.variants,
+    this.selectedVariant,
   });
 }
