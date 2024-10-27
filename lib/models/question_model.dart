0e741dd7d205fea1fb3789da1f475b5833201a48
Diyaz <diyaz.murfin1@gmail.com>
2024-10-27 12:51:50 +0500

    feat: add isQuestionAnswered field to QuestionModel

--- lib/models/question_model.dart
+++ lib/models/question_model.dart
@@ -2,10 +2,12 @@ class QuestionModel {
   final String question;
   final Map<int, String> variants;
   int? selectedVariant;
+  bool isQuestionAnswered;
 
   QuestionModel({
     required this.question,
     required this.variants,
     this.selectedVariant,
+    this.isQuestionAnswered = false,
   });
 }
