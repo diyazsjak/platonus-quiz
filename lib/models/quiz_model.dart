6b97141ef914da41e7591b74d7d63174f85d6c62
Diyaz <diyaz.murfin1@gmail.com>
2024-10-27 18:14:15 +0500

    feat: add getRightAnsweredQuestions QuizModel method

--- lib/models/quiz_model.dart
+++ lib/models/quiz_model.dart
@@ -5,4 +5,16 @@ class QuizModel {
   final List<QuestionModel> questions;
 
   QuizModel({required this.quizName, required this.questions});
+
+  int getRightAnsweredQuestions() {
+    int rightAnsweredQuestions = 0;
+
+    for (final question in questions) {
+      if (question.selectedVariant == 1) {
+        rightAnsweredQuestions++;
+      }
+    }
+
+    return rightAnsweredQuestions;
+  }
 }
