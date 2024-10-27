1f3fc9a5c9bd76f1419c5b4abd9cc17c28b26f61
Diyaz <diyaz.murfin1@gmail.com>
2024-10-27 16:43:44 +0500

    feat: shuffle and limit questions of a quiz

--- lib/bloc/quiz/quiz_bloc.dart
+++ lib/bloc/quiz/quiz_bloc.dart
@@ -3,18 +3,33 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import '../../core/failure.dart';
 import '../../core/file_parser.dart';
 import '../../models/quiz_model.dart';
+import '../../services/settings_service.dart';
 
 part 'quiz_event.dart';
 part 'quiz_state.dart';
 
 class QuizBloc extends Bloc<QuizEvent, QuizState> {
+  final settingsService = SettingsService();
+
   QuizBloc() : super(QuizInitial()) {
     on<QuizSelected>(
       (event, map) async {
         try {
           map(QuizLoadInProgress());
           final quiz = await FileParser.parseFileToQuiz(event.filePath);
-          map(QuizLoadSuccess(quiz: quiz));
+
+          final questionLimit = settingsService.questionLimit.toInt();
+          final shuffledQuestions = quiz.questions..shuffle();
+          final questions = shuffledQuestions.getRange(0, questionLimit);
+
+          map(
+            QuizLoadSuccess(
+              quiz: QuizModel(
+                quizName: quiz.quizName,
+                questions: questions.toList(),
+              ),
+            ),
+          );
         } catch (e) {
           map(QuizLoadFailure(failure: WrongQuizFormatFailure()));
         }
