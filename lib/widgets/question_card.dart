902e2aeb6392075bf03ac1fe3a614986c7285447
Diyaz <diyaz.murfin1@gmail.com>
2024-11-16 16:37:06 +0500

    style: change import method

--- lib/widgets/question_card.dart
+++ lib/widgets/question_card.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:platonus_quiz/bloc/quiz/quiz_bloc.dart';
 
+import '../bloc/quiz/quiz_bloc.dart';
 import '../models/question_model.dart';
 
 class QuestionCard extends StatefulWidget {
