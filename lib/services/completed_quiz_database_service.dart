f1418714ada80f21864ae1b197625809ae5bc517
Diyaz <diyaz.murfin1@gmail.com>
2024-12-08 19:14:54 +0500

    feat: add CompletedQuizDatabaseService

---
+++ lib/services/completed_quiz_database_service.dart
@@ -0,0 +1,22 @@
+import '../core/database.dart';
+import '../core/database_singleton.dart';
+
+class CompletedQuizDatabaseService {
+  final _database = DatabaseSingleton().database;
+
+  Future getAll(int quizId) async {
+    return await _database.managers.completedQuiz
+        .filter((f) => f.quizId.id(quizId))
+        .get();
+  }
+
+  Future<int> insert(int quizId, int questionCount, int rightQuestions) async {
+    return await _database.into(_database.completedQuiz).insert(
+          CompletedQuizCompanion.insert(
+            quizId: quizId,
+            questionCount: questionCount,
+            rightQuestions: rightQuestions,
+          ),
+        );
+  }
+}
