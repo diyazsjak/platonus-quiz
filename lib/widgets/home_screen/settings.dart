f35242964f05d9036ec48eca22db3ec43c002c4e
Diyaz <diyaz.murfin1@gmail.com>
2024-12-22 21:30:36 +0500

    refactor: extract _QuestionLimitSlider

--- lib/widgets/home_screen/settings.dart
+++ lib/widgets/home_screen/settings.dart
@@ -2,14 +2,37 @@ import 'package:flutter/material.dart';
 
 import '../../services/settings_service.dart';
 
-class Settings extends StatefulWidget {
+class Settings extends StatelessWidget {
   const Settings({super.key});
 
   @override
-  State<Settings> createState() => _SettingsState();
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      padding: const EdgeInsets.all(16),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          const Text(
+            'Settings',
+            style: TextStyle(fontSize: 20),
+            textAlign: TextAlign.center,
+          ),
+          const SizedBox(height: 16),
+          _QuestionLimitSlider(),
+        ],
+      ),
+    );
+  }
 }
 
-class _SettingsState extends State<Settings> {
+class _QuestionLimitSlider extends StatefulWidget {
+  const _QuestionLimitSlider();
+
+  @override
+  State<_QuestionLimitSlider> createState() => __QuestionLimitSliderState();
+}
+
+class __QuestionLimitSliderState extends State<_QuestionLimitSlider> {
   final settingsService = SettingsService();
   late double _questionLimitValue = settingsService.questionLimit;
 
@@ -25,41 +48,31 @@ class _SettingsState extends State<Settings> {
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      padding: const EdgeInsets.all(16),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          const Text(
-            'Settings',
-            style: TextStyle(fontSize: 20),
-            textAlign: TextAlign.center,
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(right: 16),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              const Text(
+                'Quiz questions limit',
+                style: TextStyle(fontSize: 16),
+              ),
+              Text(_questionLimitValue.toInt().toString()),
+            ],
           ),
-          const SizedBox(height: 16),
-          Padding(
-            padding: const EdgeInsets.only(right: 16.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                const Text(
-                  'Quiz questions limit',
-                  style: TextStyle(fontSize: 16),
-                ),
-                Text(_questionLimitValue.toInt().toString()),
-              ],
-            ),
-          ),
-          const SizedBox(height: 8),
-          Slider(
-            value: _questionLimitValue,
-            min: 10,
-            max: 100,
-            divisions: 18,
-            onChanged: (value) => _onQuestionLimitChanged(value),
-            onChangeEnd: (value) => _saveQuestionLimit(value),
-          ),
-        ],
-      ),
+        ),
+        const SizedBox(height: 8),
+        Slider(
+          value: _questionLimitValue,
+          min: 10,
+          max: 100,
+          divisions: 18,
+          onChanged: (value) => _onQuestionLimitChanged(value),
+          onChangeEnd: (value) => _saveQuestionLimit(value),
+        ),
+      ],
     );
   }
 }
