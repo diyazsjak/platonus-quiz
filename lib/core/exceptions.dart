c897bb0448c6e610fa1a4d57056b7cd452d7c7f5
Diyaz <diyaz.murfin1@gmail.com>
2025-01-03 20:30:58 +0500

    feat: add exceptions.dart

---
+++ lib/core/exceptions.dart
@@ -0,0 +1,5 @@
+final class WrongFileFormatException implements Exception {}
+
+final class WrongQuizFormatException implements Exception {}
+
+final class QuizAlreadyExistsException implements Exception {}
