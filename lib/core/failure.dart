925c9cea03ce62d02972a78c934d8545862f5cc5
Diyaz <diyaz.murfin1@gmail.com>
2025-01-03 20:31:10 +0500

    feat: add 2 more failures

--- lib/core/failure.dart
+++ lib/core/failure.dart
@@ -1,5 +1,9 @@
 abstract class Failure {}
 
+class WrongFileFormatFailure extends Failure {}
+
 class WrongQuizFormatFailure extends Failure {}
 
 class UnknownDatabaseFailure extends Failure {}
+
+class QuizAlreadyExistsFailure extends Failure {}
